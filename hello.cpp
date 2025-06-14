52ff4d849563eda337bfcf1c829405d28c7de146
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-14 15:49:11 +0545

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from C++ on Arch!" << endl;
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Arch Linux!\n");
+    return 0;
+}
